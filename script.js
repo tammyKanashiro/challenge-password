80f6707b00155081a202ed6de6aac99c2c303ccb
Tammy Kanashiro <tammy.kanashiro@gmail.com>
2019-09-06 15:34:27 +1000

    class included

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
 function showPass(){
-    let pass = document.getElementById("password");
-    let show = document.getElementById("show");
-    let hide = document.getElementById("hide");
-
-    if(pass.type === 'password'){
-        pass.type = "text";
-        show.style.display = "none";
-        hide.style.display = "block";
-    }else{
-        pass.type = "password";
-        show.style.display = "block";
-        hide.style.display = "none";
-    }
+    passInfo.revealedState();
 }
 
-function showStrength(t){
-    let pass = document.getElementById('password').value.length;
-    let strength = document.getElementById("strength");
-    const smile = document.getElementById("smile");
-    const neutral = document.getElementById("neutral");
-    const sad = document.getElementById("sad");
-   
-    console.log("Quantidade: " + pass);
+function showStrength(){
+    passInfo.setPass(document.getElementById('password'));
+    passInfo.stateUpdate();
+}
+
+class Login{
+    constructor(password){
+        this.password;
+    }
+
+    setPass(pass){
+        this.password = pass;
+    }
+
+    stateUpdate(){
+        let msg = document.getElementById("msg");
+        let strength = document.getElementById("strength");
+        const smile = document.getElementById("smile");
+        const neutral = document.getElementById("neutral");
+        const sad = document.getElementById("sad");
     
-    if(pass == 0){
-        strength.innerText = "";
-        smile.style.display = "none";
-        neutral.style.display = "none";
-        sad.style.display = "none";
-    }else if(pass <= 3){
-        strength.innerText = "BAD";
-        strength.style.color = "red";
-        smile.style.display = "none";
-        neutral.style.display = "none";
-        sad.style.display = "block";
-    }else if( pass > 3 && pass <= 6){
-        strength.innerText = "MEDIUM";
-        strength.style.color = "olive";
-        smile.style.display = "none";
-        neutral.style.display = "block";
-        sad.style.display = "none";
-    }else if(pass > 6){
-        strength.innerText = "GOOD";
-        strength.style.color = "green";
-        smile.style.display = "block";
-        neutral.style.display = "none";
-        sad.style.display = "none";
+        if(password.value.length == 0){
+            msg.innerText = "Original State";
+            strength.innerText = "";
+            smile.style.display = "none";
+            neutral.style.display = "none";
+            sad.style.display = "none";
+        }else if(password.value.length <= 3){
+            msg.innerText = "Strength State: ";
+            strength.innerText = "BAD";
+            strength.style.color = "red";
+            smile.style.display = "none";
+            neutral.style.display = "none";
+            sad.style.display = "block";
+        }else if( password.value.length > 3 && password.value.length <= 6){
+            msg.innerText = "Strength State: ";
+            strength.innerText = "MEDIUM";
+            strength.style.color = "olive";
+            smile.style.display = "none";
+            neutral.style.display = "block";
+            sad.style.display = "none";
+        }else if(password.value.length > 6){
+            msg.innerText = "Strength State: ";
+            strength.innerText = "GOOD";
+            strength.style.color = "green";
+            smile.style.display = "block";
+            neutral.style.display = "none";
+            sad.style.display = "none";
+        }
+    }
+
+    revealedState(){
+        let show = document.getElementById("show");
+        let hide = document.getElementById("hide");
+
+        if(password.type === 'password'){
+            password.type = "text";
+            show.style.display = "none";
+            hide.style.display = "block";
+        }else{
+            password.type = "password";
+            show.style.display = "block";
+            hide.style.display = "none";
+        }
     }
 }
+
+let passInfo = new Login(document.getElementById('password'));
